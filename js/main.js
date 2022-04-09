fa737a27b6b954127f52ff6504391c157ad121bc
Ali Koçman <34556282+Eddith@users.noreply.github.com>
2022-04-09 12:05:33 +0300

    last commit the end

--- js/main.js
+++ js/main.js
@@ -1,64 +1,104 @@
 const url = "https://www.cheapshark.com/api/1.0";
 const list = ["p-4", "sm:w-1/2", "lg:w-1/3"];
+let gameListElement = document.getElementById("gameList");
+let gameListDetailElement = document.getElementById("gameDetail");
 
-fetch(`${url}/games?title=batman&limit=60&exact=0`)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log("içerisinde batman geçen oyunların listesi", data);
-    let gameList = data;
-    let gameListLength = gameList.length;
-    let gameListElement = document.getElementById("gameList");
-    for (let i = 0; i < gameListLength; i++) {
-      let game = gameList[i];
-      let gameElement = document.createElement("div");
-      gameElement.classList.add(...list);
-      gameElement.innerHTML = `
+window.onload = () => {
+  getGameList();
+};
+
+const getGameList = () => {
+  fetch(`${url}/games?title=batman&limit=60&exact=0`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("içerisinde batman geçen oyunların listesi", data);
+      let gameList = data.sort((a, b) => a.cheapest - b.cheapest);
+      let gameListLength = gameList.length;
+      for (let i = 0; i < gameListLength; i++) {
+        let game = gameList[i];
+        let gameElement = document.createElement("div");
+        gameListElement.appendChild(gameElement);
+        gameElement.classList.add(...list);
+        gameElement.innerHTML = `
         <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-            <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+            <img class="h-64 w-full object-cover object-center"
                 src="${game.thumb}" alt="blog">
             <div class="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
-                <h2 class="text-base font-medium text-indigo-300 mb-1">October 29,
-                    2021</h2>
-                <h1 class="text-2xl font-semibold mb-3">${game.external}</h1>
-                <p class="leading-relaxed mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Aperiam modi, expedita quos doloremque autem ipsum itaque incidunt ipsam reprehenderit
-                    fuga! Dolores quisquam eius cum accusamus?</p>
+                <h1 class="text-2xl font-semibold mb-3 overflow-ellipsis overflow-hidden whitespace-nowrap">${game.external}</h1>
                 <div class="flex items-center flex-wrap ">
-                    <a onClick="detailId(${game.gameID})" class="text-indigo-300 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">Read More
-                        <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"
-                            fill="none" stroke-linecap="round" stroke-linejoin="round">
-                            <path d="M5 12h14"></path>
-                            <path d="M12 5l7 7-7 7"></path>
-                        </svg>
+                    <a onClick="detailId(${game.gameID})" class="text-red-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">
+                    Detail
                     </a>
                     <span
                         class="inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-md">
-                        ${game.cheapest}
+                        $${game.cheapest}
                     </span>
                 </div>
             </div>
         </div>
       `;
-      gameListElement.appendChild(gameElement);
-    }
-  });
+        gameListElement.appendChild(gameElement);
+      }
+    });
+};
 
-function detailId(game) {
-  fetch(`${url}/games?id=${game}`)
+const detailId = async (game) => {
+  await fetch(`${url}/games?id=${game}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log("ids verilen oyunun fiyatları", data);
+      let gameDetail = data;
+      gameListElement.parentNode.removeChild(gameListElement);
+      let gameDetailElement = document.createElement("gameDetail");
+      console.log("içerisinde seçilen oyunun detayları", gameDetail);
+      gameDetailElement.innerHTML = `
+        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+          <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+            src="${gameDetail.info.thumb}" alt="blog">
+          <div class="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
+            <h2 class="text-base font-medium text-indigo-300 mb-1">${new Date(
+              gameDetail.cheapestPriceEver.date
+            ).toLocaleDateString()}</h2>
+            <h1 class="text-2xl font-semibold mb-3">${
+              gameDetail.info.title
+            }</h1>
+            <div class="flex items-center flex-wrap ">
+              <a onClick="behind()" class="text-red-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">
+                Back
+              </a>
+              ${
+                gameDetail.deals[0].savings.split(".")[0] !== "0"
+                  ? `<span
+                class="inline-flex font-bold items-center leading-none lg:ml-auto md:ml-0 ml-auto leading-none text-md text-2xl">
+                ${gameDetail.deals[0].savings.split(".")[0]}%
+              </span>
+              &nbsp;&nbsp;&nbsp;
+              <span class="text-red-500 inline-flex items-center line-through">$${
+                gameDetail.deals[0].retailPrice
+              }
+              </span>
+              &nbsp;&nbsp;&nbsp;
+              <span class="text-2xl inline-flex items-center">
+                $${gameDetail.cheapestPriceEver.price}
+              </span>`
+                  : `
+                <span class="text-2xl lg:ml-auto md:ml-0 ml-auto leading-none text-md  inline-flex items-center">
+                  $${gameDetail.deals[0].retailPrice}
+                </span>
+                  `
+              }
+            </div>
+          </div>
+        </div>
+      `;
+      gameListDetailElement.appendChild(gameDetailElement);
     });
-}
+};
 
-fetch(`${url}/deals?id=tyTH88J0PXRvYALBjV3cNHd5Juq1qKcu4tG4lBiUCt4%3D`)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log("idi verilen fiyatın detayları", data);
-  });
+const behind = () => {
+  gameListDetailElement.parentNode.removeChild(gameListDetailElement);
+  window.location.reload();
+};
