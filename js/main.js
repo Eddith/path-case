a7ab142b2c571feb17e945ac108830bbb278c86b
Ali Koçman <34556282+Eddith@users.noreply.github.com>
2022-04-11 19:41:04 +0300

    inline style added

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 const url = "https://www.cheapshark.com/api/1.0";
-const list = ["p-4", "sm:w-1/2", "lg:w-1/3"];
 let gameListElement = document.getElementById("gameList");
 let gameListDetailElement = document.getElementById("gameDetail");
 
@@ -20,19 +19,18 @@ const getGameList = () => {
         let game = gameList[i];
         let gameElement = document.createElement("div");
         gameListElement.appendChild(gameElement);
-        gameElement.classList.add(...list);
+        gameElement.classList.add("top-container");
         gameElement.innerHTML = `
-        <div class="card-container h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-            <img class="h-64 w-full object-cover object-center"
+        <div class="card-container">
+            <img class="img-container"
                 src="${game.thumb}" alt="blog">
-            <div class="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
-                <h1 class="text-2xl font-semibold mb-3 overflow-ellipsis overflow-hidden whitespace-nowrap">${game.external}</h1>
-                <div class="flex items-center flex-wrap ">
-                    <a onClick="detailId(${game.gameID})" class="text-red-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">
+            <div class="text-detail">
+                <h1>${game.external}</h1>
+                <div class="amount-container">
+                    <a onClick="detailId(${game.gameID})">
                     Detail
                     </a>
-                    <span
-                        class="inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-md">
+                    <span>
                         $${game.cheapest}
                     </span>
                 </div>
@@ -55,37 +53,33 @@ const detailId = async (game) => {
       let gameDetailElement = document.createElement("gameDetail");
       console.log("içerisinde seçilen oyunun detayları", gameDetail);
       gameDetailElement.innerHTML = `
-        <div class="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
-          <img class="lg:h-72 md:h-48 w-full object-cover object-center"
+        <div class="card-container2">
+          <img class="img-container"
             src="${gameDetail.info.thumb}" alt="blog">
-          <div class="p-6 hover:bg-indigo-700 hover:text-white transition duration-300 ease-in">
-            <h2 class="text-base font-medium text-indigo-300 mb-1">${new Date(
+          <div class="text-detail">
+            <h2>${new Date(
               gameDetail.cheapestPriceEver.date
             ).toLocaleDateString()}</h2>
-            <h1 class="text-2xl font-semibold mb-3">${
-              gameDetail.info.title
-            }</h1>
-            <div class="flex items-center flex-wrap ">
-              <a onClick="behind()" class="text-red-500 inline-flex items-center md:mb-2 lg:mb-0 cursor-pointer">
+            <h1>${gameDetail.info.title}</h1>
+            <div class="amount-container">
+              <a onClick="behind()">
                 Back
               </a>
               ${
                 gameDetail.deals[0].savings.split(".")[0] !== "0"
                   ? `<span
-                class="inline-flex font-bold items-center leading-none lg:ml-auto md:ml-0 ml-auto leading-none text-md text-2xl">
+                class="span-1">
                 ${gameDetail.deals[0].savings.split(".")[0]}%
               </span>
               &nbsp;&nbsp;&nbsp;
-              <span class="text-red-500 inline-flex items-center line-through">$${
-                gameDetail.deals[0].retailPrice
-              }
+              <span class="span-2">$${gameDetail.deals[0].retailPrice}
               </span>
               &nbsp;&nbsp;&nbsp;
-              <span class="text-2xl inline-flex items-center">
+              <span class="span-3">
                 $${gameDetail.cheapestPriceEver.price}
               </span>`
                   : `
-                <span class="text-2xl lg:ml-auto md:ml-0 ml-auto leading-none text-md  inline-flex items-center">
+                <span class="span-4">
                   $${gameDetail.deals[0].retailPrice}
                 </span>
                   `
